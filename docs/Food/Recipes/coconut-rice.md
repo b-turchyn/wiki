47d4d3d7df9c2ef911bcf130f238cea43f5fbb0d
Brian Turchyn <brian@dj-bri-t.net>
2022-04-11 10:21:14 -0500

    Initial commit

---
+++ docs/Food/Recipes/coconut-rice.md
@@ -0,0 +1,22 @@
+---
+tags:
+  - Recipe
+---
+# Coconut Rice
+
+## Ingredients
+
+| Ingredient                     |            1x |             1.5x |
+| ------------------------------ | -------------:| ----------------:|
+| [Jasmine Rice](/Food/Ingredients/Rice#Jasmine%20Rice) |    1-1/2 cups |           2 cups |
+| Unsweetened [Coconut Milk](/Food/Ingredients/Coconut%20Milk)   | 15 oz (1 can) | 23 oz (1.5 cans) |
+| [Garlic](/Food/Ingredients/Garlic), minced             |       1 clove |         2 cloves |
+| [Salt](/Food/Ingredients/Salt)                       |         1 tsp |          1.5 tsp |
+| Water                          |         1 cup |         1.5 cups |
+
+## Instructions
+
+1. Rinse the dry [Jasmine Rice](/Food/Ingredients/Rice#Jasmine%20Rice) well until the water runs clear.
+2. In a medium-sized pot, mix together the [Jasmine Rice](/Food/Ingredients/Rice#Jasmine%20Rice), [[Coconut Milk]], [[Garlic]], [[Salt]], and water. Bring to a boil.
+3. Once the pot reaches a rolling boil, cover, reduce heat to low, and let simmer for 15 minutes.
+4. After 15 minutes, turn off heat and let sit (with lid on) for an additional 10 minutes, or until the liquid is absorbed.
---
+++ docusaurus.config.js
@@ -0,0 +1,96 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Everything I Know',
+  // tagline: 'Dinosaurs are cool',
+  url: 'https://b-turchyn.github.io/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'b-turchyn', // Usually your GitHub org/user name.
+  projectName: 'knowledge', // Usually your repo name.
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/b-turchyn/knowledge/tree/main/docs/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'My Site',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Social',
+            items: [
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/BTWritesCode',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/b-turchyn',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                href: 'https://brianturchyn.net/blog',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Brian Turchyn. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
