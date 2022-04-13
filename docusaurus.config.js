a5ab295e34985c4d909765283439aa23c4d7cab7
Brian Turchyn <brian@dj-bri-t.net>
2022-04-13 10:13:22 -0500

    Add LaTeX support

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,6 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 const sectionPrefix = require('./src/plugins/section-prefix');
 
+const math = require('remark-math');
+const katex = require('rehype-katex');
+
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Everything I Know',
@@ -29,7 +32,8 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl: 'https://github.com/b-turchyn/knowledge/tree/main/docs/',
-          remarkPlugins: [sectionPrefix],
+          remarkPlugins: [sectionPrefix, math],
+          rehypePlugins: [katex]
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -88,6 +92,14 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity: 'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    }
+  ],
   scripts: [
     {
       defer: true,
