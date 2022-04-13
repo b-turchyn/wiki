877f76dbfef187aee4607c4b8c7d087f51fa229e
Brian Turchyn <brian@dj-bri-t.net>
2022-04-12 20:50:48 -0500

    Fix config import

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,6 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-const slugger2 = require('remark-obsidian-slugger');
 const sectionPrefix = require('./src/plugins/section-prefix');
 
 /** @type {import('@docusaurus/types').Config} */
