d931f069e3b8832df3bf1e82c96e5e9930f1d8a5
Brian Turchyn <brian@dj-bri-t.net>
2022-07-27 13:46:44 -0500

    Add Java syntax highlighting support

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -106,7 +106,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['vim'],
+        additionalLanguages: ['java', 'vim'],
       },
     }),
   stylesheets: [
