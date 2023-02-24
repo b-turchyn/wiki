385c9e50971458e5180a826ff839ee87295668a2
Brian Turchyn <brian@dj-bri-t.net>
2023-02-23 20:49:12 -0600

    Improve format.sh

--- src/bin/format.sh
+++ src/bin/format.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Formats Markdown files using Prettier
-npx prettier -w $(git ls-files -mo | grep -e "\\.md$")
+npx prettier -w $(git ls-files -mo $(git rev-parse --show-cdup || echo "./")docs/ | grep -e "\\.md$")
